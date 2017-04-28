4d24bb32bbee8e06a2e40da65ef6905f8a5322a7
Manoel Ribeiro <manoelhortaribeiro@gmail.com>
2017-04-27 22:21:13 -0300

    Drops idea of a proximity sampler.

--- rle/samplers/kernel_sampler.py
+++ rle/samplers/kernel_sampler.py
@@ -4,16 +4,16 @@ from rle.samplers.sampler import Sampler
 
 
 @inherit_docstring
-class ProximitySampler(Sampler):
+class KernelSampler(Sampler):
     """ This class implements a sampler based on distance measurements. """
 
     def __init__(self,
                  features, f_names, f_types,
                  label, l_name, l_type,
-                 dist_num, dist_cat, dist_mix,
+                 kernel_fn, kernel_w, num_samples,
                  verbose=False):
         """
-        defined@Sampler Also.
+        defined@Sampler Also initializes distances metrics.
 
         :param features: defined@Sampler
         :param f_names: defined@Sampler
@@ -21,22 +21,33 @@ class ProximitySampler(Sampler):
         :param label: defined@Sampler
         :param l_name: defined@Sampler
         :param l_type: defined@Sampler
-        :param dist_num: distance metric between numerical features.
-        :param dist_cat: distance metric between categorical features.
-        :param dist_mix: distance metric between a numerical feature and a categorical feature.
+        :param kernel_fn: Function that transforms an array of distances into an  array of proximity values (floats).
+        :param kernel_w: Weight used in the kernel.
+        :param num_samples: Number of samples sampled.
         :param verbose: defined@VerboseObject
         :return: defined@Sampler
         """
+
+        self.kernel_fn, self.kernel_w, self.num_samples = kernel_fn, kernel_w, num_samples
+
         super().__init__(features, f_names, f_types,
                          label, l_name, l_type,
                          verbose)
 
-
     def sample(self,
                instance):
+        """
+        defined@Sampler
+
+        :param instance: defined@Sampler
+        :return: defined@Sampler
+        """
+
+
+
         return 0
 
     def test(self):
         return self
 
-print(ProximitySampler.__init__.__doc__)
+print(KernelSampler.sample.__doc__)
