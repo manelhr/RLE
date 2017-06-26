21a53a4f6f5bd0a498cd15b84cf2063482ee483e
manelhr <manoelhortaribeiro@gmail.com>
2017-06-26 11:31:00 -0300

    Starts to implement NSGA2 robust explanation. (1)

---
+++ tests/test_nsga2_robust_explanation.py
@@ -0,0 +1,35 @@
+from rle.robust_explanation.nsga2 import nsga2_robust_explanation
+from rle.explainers.logistic_regression_explainer import LogisticRegressionExplainer
+from rle.depicters.depicter_bar_weights import DepicterBarWeights
+from rle.samplers.gaussian_sampler import GaussianSampler
+from sklearn.model_selection import train_test_split
+from rle.explanation.explanation import Explanation
+from sklearn.ensemble import RandomForestClassifier
+import scipy.stats as stats
+import pandas as pd
+import numpy as np
+
+# Initializes peak dataset
+X = np.random.rand(800, 2)
+X[:, 0] = (X[:, 0] * 6) - 3
+X[:, 1] = X[:, 1] / 2
+y = np.array(list(map(lambda x: int(x[1] < stats.norm.pdf(x[0]) and x[0] < 0), X)))
+X[:, 0] = (X[:, 0] + 3) / 6
+X[:, 1] = X[:, 1] * 2
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+df = pd.DataFrame(X, columns=["Feature 1", "Feature 2"])
+df["Label"] = y
+
+# Initializes decision
+decision = np.array([0.34, 0.44])
+
+# Initializes explainer
+exp = Explanation(X_train, ["Feature 1", "Feature 2"], None,
+                  y_train, "Label", None,
+                  model=RandomForestClassifier(n_estimators=100),
+                  explainer=LogisticRegressionExplainer,
+                  sampler=GaussianSampler,
+                  depicter=DepicterBarWeights,
+                  destination=None)
+
+# Provides robust explanation
